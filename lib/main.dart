e32a1c81ca08d707a2db4da9b203370a829b2331
Younis-Mohammadi <artacoding2023@gmail.com>
2023-03-28 15:13:22 +0430

    assignment completed

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue[400],
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.lightBlue[900],
+        ),
+      ),
+    ),
+  );
+}
